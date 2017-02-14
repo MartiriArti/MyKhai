0e6cbe6fcd56bd9f7501b92f769a59b19b09e8aa
antonyo007h@mail.ru <opernet159>
2017-02-15 00:06:04 +0200

    //

--- app/src/main/java/tonydarko/mykhai/MainActivity.java
+++ app/src/main/java/tonydarko/mykhai/MainActivity.java
@@ -1,8 +1,9 @@
 package tonydarko.mykhai;
 
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     public Elements title;
     private ArrayAdapter<String> adapter;
-    private ArrayList<String> table;
+    private ArrayList<String> urls;
     private ListView lv;
     private TextView tv;
 
@@ -37,38 +38,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        table = new ArrayList<>();
+        urls = new ArrayList<>();
 
         toolbar.setLogo(R.mipmap.logo);
 
         lv = (ListView) findViewById(R.id.listView1);
-        tv = (TextView) findViewById(R.id.textView);
 
         ParseTitle parseTitle = new ParseTitle();
         parseTitle.execute();
 
         try {
-            final HashMap<String,String> hashMap = parseTitle.get();
+            final HashMap<String, String> hashMap = parseTitle.get();
             final ArrayList<String> arrayList = new ArrayList<>();
-            for (Map.Entry entry: hashMap.entrySet()){
+            for (Map.Entry entry : hashMap.entrySet()) {
                 arrayList.add(entry.getKey().toString());
             }
             ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_list_item_1, arrayList);
 
             lv.setAdapter(arrayAdapter);
-
+            final Intent intent = new Intent(this,TableActivity.class);
             lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                    ParseText parseText = new ParseText();
-                    parseText.execute(hashMap.get(arrayList.get(position)));
-                    try {
-                        lv.setVisibility(View.GONE);
-                        tv.setText(parseText.get());
-                    } catch (InterruptedException | ExecutionException e) {
-                        e.printStackTrace();
-                    }
+                    intent.putExtra("URL", urls.get(position));
+                    System.out.println(urls.get(position));
+                   startActivity(intent);
                 }
             });
         } catch (InterruptedException | ExecutionException e) {
@@ -76,32 +71,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onBackPressed() {
-      lv.setVisibility(View.VISIBLE);
-        tv.setVisibility(View.GONE);
-    }
-
-    public class ParseText extends AsyncTask<String, Void, String>{
+    public class ParseTitle extends AsyncTask<String, Void, HashMap<String, String>> {
+        HashMap<String, String> hashMap = new LinkedHashMap<>();
 
         @Override
-        protected String doInBackground(String... params) {
-            String str = " ";
-            try {
-                Document document = Jsoup.connect(params[0]).get();
-                Element element = document.select(".header").first();
-                str = element.text();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
-
-    public class ParseTitle extends AsyncTask<String, Void, HashMap<String,String>> {
-            HashMap<String,String> hashMap = new LinkedHashMap<>();
-        @Override
-        protected HashMap<String,String> doInBackground(String... arg) {
+        protected HashMap<String, String> doInBackground(String... arg) {
 
             Document doc;
             try {
@@ -110,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 for (Element titles : title) {
                     Element element = titles.select("a[href]").first();
                     hashMap.put(titles.text(), element.attr("abs:href"));
+                    urls.add(element.attr("abs:href"));
                 }
             } catch (IOException e) {
                 e.printStackTrace();
