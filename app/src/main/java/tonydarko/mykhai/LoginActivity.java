ecb746096825a42b46ca82c5c90c8b37295c5995
antonyo007h@mail.ru <opernet159>
2017-03-04 02:00:17 +0200

    //

--- app/src/main/java/tonydarko/mykhai/LoginActivity.java
+++ app/src/main/java/tonydarko/mykhai/LoginActivity.java
@@ -9,14 +9,13 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import tonydarko.mykhai.Utils.NetworkStatusChecker;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn;
+    Button btn, noRegBtn;
     TextInputLayout inputLogin, inputPass;
     TextInputEditText login, pass;
     Intent mainIntent;
@@ -29,69 +28,78 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         inputLogin = (TextInputLayout) findViewById(R.id.input_login);
         login = (TextInputEditText) findViewById(R.id.login);
-        inputLogin.setError("Логин пуст");
-        inputLogin.setHint("Введите логин");
 
         inputPass = (TextInputLayout) findViewById(R.id.input_pass);
-        inputPass.setError("Пароль пуст");
-        inputLogin.setHint("Введите пароль");
         pass = (TextInputEditText) findViewById(R.id.password);
 
+        noRegBtn = (Button) findViewById(R.id.no_reg_btn);
         btn = (Button) findViewById(R.id.btn);
 
         mainIntent = new Intent(LoginActivity.this, MainActivity.class);
         networkStatusChecker = new NetworkStatusChecker();
 
         setInputText();
+        noRegBtn.setOnClickListener(this);
         btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (NetworkStatusChecker.isNetworkAvailable(LoginActivity.this)) {
-            LoginActivity.this.startActivity(mainIntent);
-            overridePendingTransition(R.anim.right_in, R.anim.left_out);
-            LoginActivity.this.finish();
-        } else {
-            Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show();
+        switch (view.getId()) {
+            case R.id.no_reg_btn:
+                if (NetworkStatusChecker.isNetworkAvailable(LoginActivity.this)) {
+                    LoginActivity.this.startActivity(mainIntent);
+                    overridePendingTransition(R.anim.right_in, R.anim.left_out);
+                    LoginActivity.this.finish();
+                } else {
+                    Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            case R.id.btn:
+                if (NetworkStatusChecker.isNetworkAvailable(LoginActivity.this)) {
+                    if (login.getText().length() != 0 & pass.getText().length() != 0) {
+                        LoginActivity.this.startActivity(mainIntent);
+                        overridePendingTransition(R.anim.right_in, R.anim.left_out);
+                        LoginActivity.this.finish();
+                    }
+                    Toast.makeText(this, "Логин или пароль пуст", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG).show();
+                }
+                break;
         }
     }
 
-    public void setInputText(){
+    public void setInputText() {
         login.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.length() == 0 ) {
+                if (charSequence.length() == 0) {
                     btn.setEnabled(false);
                     inputLogin.setError("Логин пуст");
-                } else {
+                } else if (charSequence.length() != 0){
                     btn.setEnabled(true);
                     inputLogin.setErrorEnabled(false);
                 }
-                if (charSequence.length() >= 14 ) {
+                if (charSequence.length() >= 14) {
                     btn.setEnabled(false);
                     inputLogin.setError("Не допустимая длина логина");
-                } else {
-                    btn.setEnabled(true);
-                    inputLogin.setErrorEnabled(false);
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
             }
         });
 
         pass.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
@@ -99,16 +107,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 if (charSequence.length() == 0) {
                     btn.setEnabled(false);
                     inputPass.setError("Пароль пуст");
-                } else {
+                } else if (charSequence.length() != 0){
                     btn.setEnabled(true);
                     inputPass.setErrorEnabled(false);
                 }
                 if (charSequence.length() >= 14) {
                     btn.setEnabled(false);
                     inputPass.setError("Не допустимая длина пароля");
-                } else {
-                    btn.setEnabled(true);
-                    inputPass.setErrorEnabled(false);
                 }
             }
 
